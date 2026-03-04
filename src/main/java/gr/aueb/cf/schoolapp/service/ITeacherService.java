bbb54a9940d38a4bcc011627027c98572970dcd7
Στέφανος Μότσος <stefanosmotsos01@gmail.com>
2026-03-04 17:22:51 +0200

    Added the complete project

--- src/main/java/gr/aueb/cf/schoolapp/service/ITeacherService.java
+++ src/main/java/gr/aueb/cf/schoolapp/service/ITeacherService.java
@@ -2,12 +2,29 @@ package gr.aueb.cf.schoolapp.service;
 
 import gr.aueb.cf.schoolapp.core.exceptions.EntityAlreadyExistsException;
 import gr.aueb.cf.schoolapp.core.exceptions.EntityInvalidArgumentException;
+import gr.aueb.cf.schoolapp.core.exceptions.EntityNotFoundException;
+import gr.aueb.cf.schoolapp.dto.TeacherEditDTO;
 import gr.aueb.cf.schoolapp.dto.TeacherInsertDTO;
 import gr.aueb.cf.schoolapp.dto.TeacherReadOnlyDTO;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.UUID;
 
 public interface ITeacherService {
     TeacherReadOnlyDTO saveTeacher(TeacherInsertDTO teacherInsertDTO)
             throws EntityAlreadyExistsException, EntityInvalidArgumentException;
 
+    public boolean isTeacherExists(String uuid);
 
+    Page<TeacherReadOnlyDTO> getPaginatedTeachersDeletedFalse(Pageable pageable);
+    Page<TeacherReadOnlyDTO> getPaginatedTeachers(Pageable pageable);
+
+    TeacherReadOnlyDTO updateTeacher(TeacherEditDTO teacherEditDTO)
+            throws EntityNotFoundException, EntityAlreadyExistsException, EntityInvalidArgumentException;
+
+    TeacherEditDTO getTeacherByUUID(UUID uuid) throws EntityNotFoundException;
+
+    TeacherReadOnlyDTO deleteTeacherByUUID(UUID uuid) throws EntityNotFoundException;
+    public TeacherEditDTO getTeacherByUUIDDeletedFalse(UUID uuid) throws EntityNotFoundException;
 }
